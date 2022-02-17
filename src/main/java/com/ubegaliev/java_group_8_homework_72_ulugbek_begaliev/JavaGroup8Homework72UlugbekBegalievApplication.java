aeaa43be9028184bd8a92ff461ef5fcf1f64b4a1
UlugbekBegaliev <uluga.begaliev@gmail.com>
2022-02-18 00:28:25 +0600

    init commit

---
+++ src/main/java/com/ubegaliev/java_group_8_homework_72_ulugbek_begaliev/JavaGroup8Homework72UlugbekBegalievApplication.java
@@ -0,0 +1,13 @@
+package com.ubegaliev.java_group_8_homework_72_ulugbek_begaliev;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JavaGroup8Homework72UlugbekBegalievApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JavaGroup8Homework72UlugbekBegalievApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/ubegaliev/java_group_8_homework_72_ulugbek_begaliev/JavaGroup8Homework72UlugbekBegalievApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ubegaliev.java_group_8_homework_72_ulugbek_begaliev;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JavaGroup8Homework72UlugbekBegalievApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
