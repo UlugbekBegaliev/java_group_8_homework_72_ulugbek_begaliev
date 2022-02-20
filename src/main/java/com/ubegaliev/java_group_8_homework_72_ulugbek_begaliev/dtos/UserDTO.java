5c25651a0c3b7e90a9334710aa7656e28b27f2c9
UlugbekBegaliev <uluga.begaliev@gmail.com>
2022-02-21 02:09:27 +0600

    added UserDTO class

--- src/main/java/com/ubegaliev/java_group_8_homework_72_ulugbek_begaliev/dtos/UserDTO.java
+++ src/main/java/com/ubegaliev/java_group_8_homework_72_ulugbek_begaliev/dtos/UserDTO.java
@@ -1,9 +1,23 @@
 package com.ubegaliev.java_group_8_homework_72_ulugbek_begaliev.dtos;
 
-import lombok.Builder;
-import lombok.Data;
+import com.ubegaliev.java_group_8_homework_72_ulugbek_begaliev.models.User;
+import lombok.*;
 
 @Data
-@Builder
+@Builder(access = AccessLevel.PRIVATE)
+@AllArgsConstructor(access = AccessLevel.PRIVATE)
+@NoArgsConstructor(access = AccessLevel.PRIVATE, force = true)
 public class UserDTO {
+
+    private int id;
+    private String email;
+    private String username;
+
+    public static UserDTO from(User user){
+        return UserDTO.builder()
+                .id(user.getId())
+                .email(user.getEmail())
+                .username(user.getUsername())
+                .build();
+    }
 }
