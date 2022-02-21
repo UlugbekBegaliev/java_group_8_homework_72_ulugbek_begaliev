8d78f19e40b649dfad0014a1defab1e4332f1f6f
UlugbekBegaliev <uluga.begaliev@gmail.com>
2022-02-21 17:35:08 +0600

    added files for further editing

--- src/main/java/com/ubegaliev/java_group_8_homework_72_ulugbek_begaliev/JavaGroup8Homework72UlugbekBegalievApplication.java
+++ src/main/java/com/ubegaliev/java_group_8_homework_72_ulugbek_begaliev/JavaGroup8Homework72UlugbekBegalievApplication.java
@@ -9,5 +9,4 @@ public class JavaGroup8Homework72UlugbekBegalievApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(JavaGroup8Homework72UlugbekBegalievApplication.class, args);
 	}
-
 }
