0b5562298a64b78647479becdf5e8b76080472df
UlugbekBegaliev <uluga.begaliev@gmail.com>
2022-02-21 16:01:13 +0600

    added getByEmail method in UserService

--- src/main/java/com/ubegaliev/java_group_8_homework_72_ulugbek_begaliev/services/UserService.java
+++ src/main/java/com/ubegaliev/java_group_8_homework_72_ulugbek_begaliev/services/UserService.java
@@ -1,5 +1,6 @@
 package com.ubegaliev.java_group_8_homework_72_ulugbek_begaliev.services;
 
+import com.ubegaliev.java_group_8_homework_72_ulugbek_begaliev.dtos.UserDTO;
 import com.ubegaliev.java_group_8_homework_72_ulugbek_begaliev.repositories.UserRepository;
 import lombok.AllArgsConstructor;
 import org.springframework.stereotype.Service;
@@ -9,4 +10,11 @@ import org.springframework.stereotype.Service;
 public class UserService {
 
     private final UserRepository userRepository;
+
+    public UserDTO getByEmail(String email){
+
+        var user = userRepository.finByEmail(email).get();
+
+        return UserDTO.from(user);
+    }
 }
