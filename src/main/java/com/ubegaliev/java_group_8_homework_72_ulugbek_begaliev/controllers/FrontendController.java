7da5d9889de63ff5b23821cb8c947dd37aee322c
UlugbekBegaliev <uluga.begaliev@gmail.com>
2022-02-21 16:21:16 +0600

    added pageUserProfile in FrontendController

--- src/main/java/com/ubegaliev/java_group_8_homework_72_ulugbek_begaliev/controllers/FrontendController.java
+++ src/main/java/com/ubegaliev/java_group_8_homework_72_ulugbek_begaliev/controllers/FrontendController.java
@@ -1,8 +1,24 @@
 package com.ubegaliev.java_group_8_homework_72_ulugbek_begaliev.controllers;
 
+import com.ubegaliev.java_group_8_homework_72_ulugbek_begaliev.services.UserService;
+import lombok.AllArgsConstructor;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+
+import java.security.Principal;
 
 @Controller
+@AllArgsConstructor
 public class FrontendController {
 
+    private final UserService userService;
+
+    @GetMapping("/profile")
+    public String pageUserProfile(Model model, Principal principal){
+
+        var user = userService.getByEmail(principal.getName());
+        model.addAttribute("dto", user);
+        return "profile";
+    }
 }
