f488b7a41394c8b9f013525088ae97e7e59bcd66
UlugbekBegaliev <uluga.begaliev@gmail.com>
2022-02-21 16:54:34 +0600

    added two methods

--- src/main/java/com/ubegaliev/java_group_8_homework_72_ulugbek_begaliev/controllers/MessageController.java
+++ src/main/java/com/ubegaliev/java_group_8_homework_72_ulugbek_begaliev/controllers/MessageController.java
@@ -1,9 +1,15 @@
 package com.ubegaliev.java_group_8_homework_72_ulugbek_begaliev.controllers;
 
+import com.ubegaliev.java_group_8_homework_72_ulugbek_begaliev.dtos.MessageDTO;
+import com.ubegaliev.java_group_8_homework_72_ulugbek_begaliev.models.Message;
 import com.ubegaliev.java_group_8_homework_72_ulugbek_begaliev.services.MessageService;
 import lombok.AllArgsConstructor;
+import org.springframework.data.domain.Pageable;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
 
 @RestController
 @AllArgsConstructor
@@ -11,4 +17,9 @@ import org.springframework.web.bind.annotation.RestController;
 public class MessageController {
 
     private final MessageService messageService;
+
+    @GetMapping
+    public List<MessageDTO> findMessages(Pageable pageable){
+        return messageService.getAll(pageable).getContent();
+    }
 }
--- src/main/java/com/ubegaliev/java_group_8_homework_72_ulugbek_begaliev/services/MessageService.java
+++ src/main/java/com/ubegaliev/java_group_8_homework_72_ulugbek_begaliev/services/MessageService.java
@@ -1,8 +1,12 @@
 package com.ubegaliev.java_group_8_homework_72_ulugbek_begaliev.services;
 
+import com.ubegaliev.java_group_8_homework_72_ulugbek_begaliev.dtos.MessageDTO;
+import com.ubegaliev.java_group_8_homework_72_ulugbek_begaliev.models.Message;
 import com.ubegaliev.java_group_8_homework_72_ulugbek_begaliev.repositories.MessageRepository;
 import com.ubegaliev.java_group_8_homework_72_ulugbek_begaliev.repositories.UserRepository;
 import lombok.AllArgsConstructor;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
 
 @Service
@@ -11,4 +15,9 @@ public class MessageService {
 
     private final MessageRepository messageRepository;
     private final UserRepository userRepository;
+
+    public Page<MessageDTO> getAll(Pageable pageable){
+        Page<Message> messages = messageRepository.findAll(pageable);
+        return messages.map(MessageDTO::from);
+    }
 }
